f22ece9e72f77ad57d39b1b4c2ff6d94d1b4f772
Bardi Robert <bardirobert1@gmail.com>
2025-03-25 19:45:44 +0200

    merge textura

--- project_1/project_1.runs/impl_2/vga.tcl
+++ project_1/project_1.runs/impl_2/vga.tcl
@@ -17,7 +17,7 @@ proc create_report { reportName command } {
   }
 }
 namespace eval ::optrace {
-  variable script "C:/Users/bardi/Desktop/VHDL_Civilization/project_1/project_1.runs/impl_2/vga.tcl"
+  variable script "C:/Users/bardi/Desktop/backup/VHDL_Civilization/project_1/project_1.runs/impl_2/vga.tcl"
   variable category "vivado_impl"
 }
 
@@ -115,8 +115,6 @@ proc step_failed { step } {
 OPTRACE "impl_2" END { }
 }
 
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 
 OPTRACE "impl_2" START { ROLLUP_1 }
 OPTRACE "Phase: Write Bitstream" START { ROLLUP_AUTO }
@@ -125,12 +123,11 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 8
   set_param xicom.use_bs_reader 1
   set_param runs.launchOptions { -jobs 32  }
   open_checkpoint vga_routed.dcp
-  set_property webtalk.parent_dir C:/Users/bardi/Desktop/VHDL_Civilization/project_1/project_1.cache/wt [current_project]
+  set_property webtalk.parent_dir C:/Users/bardi/Desktop/backup/VHDL_Civilization/project_1/project_1.cache/wt [current_project]
 set_property TOP vga [current_fileset]
 OPTRACE "read constraints: write_bitstream" START { }
 OPTRACE "read constraints: write_bitstream" END { }
